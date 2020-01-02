7d764b65be9308bc0b5909f2d75d6349c12f51fb
Mark Boyd <markdavidboyd@gmail.com>
2020-01-02 12:57:26 -0500

    add @Expose annotations to SchemaLocations private members

--- message_parser/src/main/java/cumulus_message_adapter/message_parser/SchemaLocations.java
+++ message_parser/src/main/java/cumulus_message_adapter/message_parser/SchemaLocations.java
@@ -1,12 +1,17 @@
 package cumulus_message_adapter.message_parser;
 
+import com.google.gson.annotations.Expose;
+
 /**
  * Class to hold the schema file locations for JSON serialization by GSON
  */
 public class SchemaLocations
 {
+    @Expose
     private final String input;
+    @Expose
     private final String output;
+    @Expose
     private final String config;
 
     public SchemaLocations(String inputSchemaLocation, String outputSchemaLocation, String configSchemaLocation)

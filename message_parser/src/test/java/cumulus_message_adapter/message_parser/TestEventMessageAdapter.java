ca682146238cb247c66b354357ce1149b3a22c81
Mark Boyd <markdavidboyd@gmail.com>
2020-01-02 12:11:40 -0500

    add explicit type for JSON string to map converstion

--- message_parser/src/test/java/cumulus_message_adapter/message_parser/TestEventMessageAdapter.java
+++ message_parser/src/test/java/cumulus_message_adapter/message_parser/TestEventMessageAdapter.java
@@ -1,7 +1,5 @@
 package cumulus_message_adapter.message_parser;
 
-import com.amazonaws.services.lambda.runtime.Context; 
-
 import com.google.gson.Gson;
 import java.util.Map;
 
@@ -12,7 +10,7 @@ public class TestEventMessageAdapter extends MessageAdapter
 {
     /**
      * Return the event portion of the input JSON for testing purposes
-     * 
+     *
      * @param messageAdapterFunction - 'loadAndUpdateRemoteEvent', 'loadNestedEvent', or 'createNextEvent'
      * @param inputJson - argument to message adapter function. Json that contains all of the params.
      * @return inputJson to test JSON conversions
@@ -22,8 +20,8 @@ public class TestEventMessageAdapter extends MessageAdapter
     {
         Gson gson = new Gson();
 
-        Map map = gson.fromJson(inputJson, Map.class);
-    
-        return gson.toJson(map.get("event"));
+        Map<String, Object> inputMap = AdapterUtilities.convertJsonStringToMap(inputJson);
+
+        return gson.toJson(inputMap.get("event"));
     }
 }

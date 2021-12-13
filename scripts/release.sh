f7c0762744a028fe25125637ac328f6efec5def5
Mark Boyd <markdavidboyd@gmail.com>
2021-12-13 10:36:59 -0500

    update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,30 +5,29 @@ VERSION_NUMBER=$(cd message_parser && mvn help:evaluate -Dexpression=project.ver
 export VERSION_NUMBER
 export VERSION_TAG="v$VERSION_NUMBER"
 
-echo "$VERSION_TAG"
-# export LATEST_TAG=$(curl -H \
-#   "Authorization: token $GITHUB_TOKEN" \
-#   https://api.github.com/repos/nasa/cumulus-message-adapter-java/tags | jq --raw-output '.[0].name')
+export LATEST_TAG=$(curl -H \
+  "Authorization: token $GITHUB_TOKEN" \
+  https://api.github.com/repos/nasa/cumulus-message-adapter-java/tags | jq --raw-output '.[0].name')
 
-# if [ "$VERSION_TAG" != "$LATEST_TAG" ]; then
-#   echo "tag does not exist for version $VERSION_TAG, creating tag"
+if [ "$VERSION_TAG" != "$LATEST_TAG" ]; then
+  echo "tag does not exist for version $VERSION_TAG, creating tag"
 
-#   # create git tag
-#   git tag -a "$VERSION_TAG" -m "$VERSION_TAG" || echo "$VERSION_TAG already exists"
-#   git push origin "$VERSION_TAG"
-# fi
+  # create git tag
+  # git tag -a "$VERSION_TAG" -m "$VERSION_TAG" || echo "$VERSION_TAG already exists"
+  # git push origin "$VERSION_TAG"
+fi
 
-# export RELEASE_URL=$(curl -H \
-#   "Authorization: token $GITHUB_TOKEN" \
-#   https://api.github.com/repos/nasa/cumulus-message-adapter-java/releases/tags/$VERSION_TAG | jq --raw-output '.url // ""')
+export RELEASE_URL=$(curl -H \
+  "Authorization: token $GITHUB_TOKEN" \
+  https://api.github.com/repos/nasa/cumulus-message-adapter-java/releases/tags/$VERSION_TAG | jq --raw-output '.url // ""')
 
-# if [ -z "$RELEASE_URL" ]; then
-#   echo "release does not exist"
+if [ -z "$RELEASE_URL" ]; then
+  echo "release does not exist"
 
-#   curl -H \
-#     "Authorization: token $GITHUB_TOKEN" \
-#     -d "{\"tag_name\": \"$VERSION_TAG\", \"name\": \"$VERSION_TAG\", \"body\": \"Release $VERSION_TAG\" }"\
-#     -H "Content-Type: application/json"\
-#     -X POST \
-#     https://api.github.com/repos/nasa/cumulus-message-adapter-java/releases
-# fi
+  # curl -H \
+  #   "Authorization: token $GITHUB_TOKEN" \
+  #   -d "{\"tag_name\": \"$VERSION_TAG\", \"name\": \"$VERSION_TAG\", \"body\": \"Release $VERSION_TAG\" }"\
+  #   -H "Content-Type: application/json"\
+  #   -X POST \
+  #   https://api.github.com/repos/nasa/cumulus-message-adapter-java/releases
+fi

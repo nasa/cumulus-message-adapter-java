a182e20aa1587f06fafe95914cf8a32c11b188f8
Mark Boyd <markdavidboyd@gmail.com>
2021-12-13 10:37:22 -0500

    update release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,9 +5,10 @@ VERSION_NUMBER=$(cd message_parser && mvn help:evaluate -Dexpression=project.ver
 export VERSION_NUMBER
 export VERSION_TAG="v$VERSION_NUMBER"
 
-export LATEST_TAG=$(curl -H \
+LATEST_TAG=$(curl -H \
   "Authorization: token $GITHUB_TOKEN" \
   https://api.github.com/repos/nasa/cumulus-message-adapter-java/tags | jq --raw-output '.[0].name')
+export LATEST_TAG
 
 if [ "$VERSION_TAG" != "$LATEST_TAG" ]; then
   echo "tag does not exist for version $VERSION_TAG, creating tag"
@@ -17,9 +18,10 @@ if [ "$VERSION_TAG" != "$LATEST_TAG" ]; then
   # git push origin "$VERSION_TAG"
 fi
 
-export RELEASE_URL=$(curl -H \
+RELEASE_URL=$(curl -H \
   "Authorization: token $GITHUB_TOKEN" \
   https://api.github.com/repos/nasa/cumulus-message-adapter-java/releases/tags/$VERSION_TAG | jq --raw-output '.url // ""')
+export RELEASE_URL
 
 if [ -z "$RELEASE_URL" ]; then
   echo "release does not exist"

c8b951ddc3bcf6b6f021cc365024a203a8f260cc
Mark Boyd <markdavidboyd@gmail.com>
2021-12-13 11:02:08 -0500

    re-enable release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -14,8 +14,8 @@ if [ "$VERSION_TAG" != "$LATEST_TAG" ]; then
   echo "tag does not exist for version $VERSION_TAG, creating tag"
 
   # create git tag
-  # git tag -a "$VERSION_TAG" -m "$VERSION_TAG" || echo "$VERSION_TAG already exists"
-  # git push origin "$VERSION_TAG"
+  git tag -a "$VERSION_TAG" -m "$VERSION_TAG" || echo "$VERSION_TAG already exists"
+  git push origin "$VERSION_TAG"
 fi
 
 RELEASE_URL=$(curl -H \
@@ -26,10 +26,10 @@ export RELEASE_URL
 if [ -z "$RELEASE_URL" ]; then
   echo "release does not exist"
 
-  # curl -H \
-  #   "Authorization: token $GITHUB_TOKEN" \
-  #   -d "{\"tag_name\": \"$VERSION_TAG\", \"name\": \"$VERSION_TAG\", \"body\": \"Release $VERSION_TAG\" }"\
-  #   -H "Content-Type: application/json"\
-  #   -X POST \
-  #   https://api.github.com/repos/nasa/cumulus-message-adapter-java/releases
+  curl -H \
+    "Authorization: token $GITHUB_TOKEN" \
+    -d "{\"tag_name\": \"$VERSION_TAG\", \"name\": \"$VERSION_TAG\", \"body\": \"Release $VERSION_TAG\" }"\
+    -H "Content-Type: application/json"\
+    -X POST \
+    https://api.github.com/repos/nasa/cumulus-message-adapter-java/releases
 fi

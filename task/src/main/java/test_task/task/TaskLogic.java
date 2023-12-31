b7fcc47000d38dedc7d029b7d3bb4f70e061b38e
John H. Freeman <john@freemania.pro>
2023-12-31 01:37:10 -0500

    Null test for task
    added test for null pointer exception in case no parameter is given
    PerformFunction of ITask interface is annotated with an Override
    indicating it is a method implementation of the ITask interface

--- task/src/main/java/test_task/task/TaskLogic.java
+++ task/src/main/java/test_task/task/TaskLogic.java
@@ -71,7 +71,7 @@ public class TaskLogic implements ITask
      * @param context - AWS Lambda context
      * @return JSON string
      */
-    public String PerformFunction(String input, Context context)
+    public String PerformFunction(String input)
     {
         AdapterLogger.LogInfo("Business logic input: " + input);
 
---
+++ task/src/test/java/test_task/task/TaskLogicTest.java
@@ -0,0 +1,24 @@
+package test_task.task;
+
+//import com.google.gson.JsonSyntaxException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TaskLogicTest
+{
+    static TaskLogic taskLogicObject;
+
+    @BeforeAll
+    public static void InitializeTaskTest()
+    {
+        taskLogicObject = new TaskLogic();
+    }
+
+    @Test
+    public void taskLogicGetTopicArnIsNullReturn()
+    {
+        assertThrows(NullPointerException.class, () -> TaskLogicTest.taskLogicObject.PerformFunction(null));
+    }
+}

008382fe943358485cb2918d57ac5d8aa8539042
John H. Freeman <john@freemania.pro>
2024-01-27 19:12:42 -0500

    Updated tests parent pom (#6)
    * added parent pom file
    
    * Update build-and-test.yml

--- message_parser/src/main/java/cumulus_message_adapter/message_parser/MessageParser.java
+++ message_parser/src/main/java/cumulus_message_adapter/message_parser/MessageParser.java
@@ -1,7 +1,6 @@
 package cumulus_message_adapter.message_parser;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
